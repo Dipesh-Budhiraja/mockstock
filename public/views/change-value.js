7e16aac5c2f2113ca9d3736add5146b7b54bd326
Dipesh-Budhiraja <25dipesh98@gmail.com>
2018-02-02 01:49:23 +0530

    ye lo

--- public/views/change-value.js
+++ public/views/change-value.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
+    var curPrice = $(".curPrice")[0].textContent;
+    // console.log(curPrice);
     
     $(".amount-input").keydown(function(event){
-        var value = event.which - 48;
-        console.log(value);
-        
+        setTimeout(() => {
+            var amount = event.currentTarget.value;
+            // console.log(amount);
+            var quantity = amount/curPrice;
+            console.log(quantity);
+            $(".change-value")[0].textContent = quantity;
+        }, 0002);
     });
 });
